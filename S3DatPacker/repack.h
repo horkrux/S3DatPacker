a9af78399cfceb616b76d4534e2f8a9cf7f684cb
horkrux <dominik.a.vollmer@web.de>
2019-03-28 19:18:42 +0100

    forgot headers

--- S3DatPacker/repack.h
+++ S3DatPacker/repack.h
@@ -7,5 +7,5 @@ static int write_main_header();
 static int write_sub_header(const file_type, const int);
 static int write_section_simple(const file_type, const char*, const int, char**);
 static int write_section_menu(const char*, const int, char**, int16_t*, int16_t*);
-static int write_section_texture(const char*, const int, char**, int8_t*, int8_t*);
+static int write_section_texture(const char*, const int, char**, int8_t*);
 static int write_section(const file_type, const char*, const int, char**, int*, int16_t*, int16_t*);
--- S3DatPacker/unpack.h
+++ S3DatPacker/unpack.h
@@ -1,3 +1,4 @@
 #pragma once
 
+int unpack_gfx(char*, char*, char*, s3_dat_file_format);
 int determine_pixelformat(s3_dat_main_header);
